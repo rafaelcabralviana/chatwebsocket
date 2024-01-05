1910340d782ff7e002bec65e2370fc70d3d675ae
Rafael C Viana <30372591+rafaelcabralviana@users.noreply.github.com>
2024-01-05 11:25:00 -0300

    Update script.js render server

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -87,7 +87,7 @@ const handleLogin = (event) => {
     login.style.display = "none"
     chat.style.display = "flex"
 
-    websocket = new WebSocket("ws://localhost:8080")
+    websocket = new WebSocket("wss://chat-backend-lt5f.onrender.com")
 
     websocket.onmessage = processMessage
 
